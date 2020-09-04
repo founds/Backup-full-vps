9d2a46d6146ace531a51e4d22a5f09b518b1ef9b
founds <founds@gmail.com>
2020-09-04 22:30:30 +0200

    Se añade fecha y nombre del host al nombre del backup.

---
+++ backup-full.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#-*- ENCODING: UTF-8 -*-
+
+# Script Bash para realizar una copia de seguridad completa del sistema.
+
+# Variables
+
+# Fecha.
+FECHA=`date +"%d%m%Y"`
+
+# Hostname
+HOST=$(hostname|cut -f1 -d.)
+
+# Directorio donde guardar la copia de seguridad
+DIR_BK='/home/backups'
+
+tar -cvpzf $DIR_BK/backup-$HOST-FULL-$FECHA.tar.gz --exclude=/home/backups --exclude=/mnt --exclude=/sys --exclude=dev/pts --exclude=/proc --exclude=/lost+found --one-file-system /
