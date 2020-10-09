4dbd483119234fdee9df2f2f781f61ae5dd1b922
founds <founds@gmail.com>
2020-10-09 17:13:53 +0200

    Se comprueba que exista la carpeta de destino y en caso contrario se crea

--- backup-full.sh
+++ backup-full.sh
@@ -15,4 +15,11 @@ HOST=$(hostname|cut -f1 -d.)
 # Directorio donde guardar la copia de seguridad
 DIR_BK='/home/backups'
 
+# Comprobar que exista el directorio de destino
+if [ ! -d $DIR_BK ]
+then
+    mkdir $DIR_BK
+    
 tar -cvpzf $DIR_BK/backup-$HOST-FULL-$FECHA.tar.gz --exclude=/home/backups --exclude=/mnt --exclude=/sys --exclude=dev/pts --exclude=/proc --exclude=/lost+found --one-file-system /
+
+
